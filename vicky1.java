87d0d475ada387cc71d6b97d02d18723432507de
VIKAS MC <vickymc382@gmail.com>
2026-09-06 19:32:54 +0530

    Added vicky1 java program

---
+++ vicky1.java
@@ -0,0 +1,17 @@
+ import  java.util.Scanner;
+ class vicky1{
+    public static void main( String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a number");
+        int n = sc.nextInt();
+        if (n<143)
+        {
+            System.out.println("I LOVE YOU");
+        }
+        else
+        {
+            System.out.println("I LOVE  YOU MORE ");
+        }
+    }
+
+ }
